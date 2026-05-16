2558d24eeef26da1784a265404849832272db6df
OwO <spby59471@gmail.com>
2026-05-16 20:04:21 +0800

    test: deliberate failure for homework report

--- test/app.test.ts
+++ test/app.test.ts
@@ -21,7 +21,7 @@ describe('Fastify app', () => {
       url: '/'
     });
 
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(404);
     expect(response.json().message).toBe('CI/CD Lab Fastify app is running');
     await app.close();
   });
